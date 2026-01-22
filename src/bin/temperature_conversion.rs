d6489e5b76b23fecc0dd0793f7822b54ac904364
ByteHackerRebel <muhammadahaadd@gmail.com>
2026-01-22 21:42:00 +0500

    fahrenheit to celsius

--- src/bin/temperature_conversion.rs
+++ src/bin/temperature_conversion.rs
@@ -1,24 +1,22 @@
 use std::io;
 
-fn main(){
-
+fn main() {
     println!("1 to convert temperature Fahrenheit into Celcius");
     println!("2 to convert temperature Celcius into Fahrenheit");
 
-    let mut input= String::new();
+    let mut input = String::new();
 
     io::stdin()
-    .read_line(&mut input)
-    .expect("Failed to read number");
+        .read_line(&mut input)
+        .expect("Failed to read number");
 
-    let num:i32 =match input.trim().parse(){
-        Ok(num)=>num,
-        Err(_)=>{
+    let num: i32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
             println!("Invalid number");
             return;
         }
     };
-
 
     match num {
         1 => fahrenheit_to_celsius(),
@@ -28,24 +26,48 @@ fn main(){
             return;
         }
     };
-
-
 }
 
-fn fahrenheit_to_celsius(){}
+// ================================
+// function that convert temperature fahrenheit into celsius
+// ================================
 
+fn fahrenheit_to_celsius() {
+    println!("Enter temperature in fahrenheit");
 
-fn celsius_to_fahrenheit(){
+    let mut fahrenheit = String::new();
 
+    io::stdin()
+        .read_line(&mut fahrenheit)
+        .expect("Failed to read number");
+
+    let fahrenheit: f64 = match fahrenheit.trim().parse() {
+        Ok(n) => n,
+        Err(_) => {
+            println!("Invalid number");
+            return;
+        }
+    };
+
+    let celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+
+    println!("Celsius = {celsius}");
+}
+
+// ================================
+// function that convert temperature celsius into fahrenheit
+// ================================
+
+fn celsius_to_fahrenheit() {
     println!("Enter temperature in Celsius");
 
     let mut celsius = String::new();
 
     io::stdin()
-    .read_line(&mut celsius)
-    .expect("Failed to read number");
+        .read_line(&mut celsius)
+        .expect("Failed to read number");
 
-    let celsius: f64 = match celsius.trim().parse(){
+    let celsius: f64 = match celsius.trim().parse() {
         Ok(n) => n,
         Err(_) => {
             println!("Invalid number");
