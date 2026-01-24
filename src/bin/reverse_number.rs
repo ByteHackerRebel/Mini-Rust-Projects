772e366df3445ed254049343c9ff33c61d2239ba
ByteHackerRebel <muhammadahaadd@gmail.com>
2026-01-24 19:03:42 +0500

    fix: update prompt and add function of reverse number

--- src/bin/reverse_number.rs
+++ src/bin/reverse_number.rs
@@ -3,7 +3,7 @@ use std::io;
 fn main(){
 
      // Prompt the user
-    println!("Enter a number to calculate factorial:");
+    println!("Enter a number to calculate reverse:");
 
     // Mutable string to store input
     let mut input = String::new();
@@ -14,7 +14,7 @@ fn main(){
         .expect("Failed to read number");
 
     // Parse input into an integer
-    let mut num: i32 = match input.trim().parse() {
+    let num: i32 = match input.trim().parse() {
         Ok(n) => n,
         Err(_) => {
             println!("Invalid number! Please enter a valid integer.");
@@ -22,16 +22,22 @@ fn main(){
         }
     };
   
-    let mut reverse=0;   
-  
+    rev_num(num);
+    
+}
 
-    while num != 0{
-        let digit = num % 10; // Get last digit
+
+fn rev_num(num: i32){
+
+        let mut reverse=0;     
+        let mut o_num=num;
+
+    while o_num != 0{
+        let digit = o_num % 10; // Get last digit
         reverse = reverse * 10 + digit; // APpend digit to reverse
-        num = num / 10; // remove last digit
+        o_num = o_num / 10; // remove last digit
 
     }
     
     println!("Reverse number is {reverse}");
-    
 }
